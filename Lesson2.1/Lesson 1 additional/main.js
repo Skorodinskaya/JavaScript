aa99ecf9e676927b75e5c6000616794aee2e487d
Skorodinskaya <skorodinskaya01@gmail.com>
2021-07-26 09:22:11 +0300

    Lesson 2 homework additional changes

--- Lesson2.1/Lesson 1 additional/main.js
+++ Lesson2.1/Lesson 1 additional/main.js
@@ -124,5 +124,48 @@ for (i=0; i<array7.length; i++) {
 console.log(mathe);
 
 // 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+let array8 = [23, 53, 2, 4, 6, 8];
+let array9 = [];
+for (i = 0; i<array8.length; i++) {
+ array9.push(array8[i]*5)
+}
+console.log(array9);
+
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
-//
+let array10 = [2, true, 'ana', 4, false, 'chanel', 5];
+let array11 = [];
+for (i=0; i<array10.length; i++) {
+    if (typeof array10[i] === `number`) {
+        array11.push(array10[i])
+    }
+    console.log(array11);
+}
+
+// - Дано 2 масиви з рівною кількістю об'єктів. З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+// Записати цей об'єкт в новий масив
+
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false}
+];
+
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+];
+
+let usersWithCities = [];
+
+for (const user of usersWithId) {
+    for (const city of citiesWithId) {
+        if (user.id === city.user_id) {
+            user.address = city;
+            usersWithCities.push (user)
+        }
+    }
+}
+console.log(usersWithCities);
