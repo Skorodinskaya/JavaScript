8a309754d6f7e5a7011dcbbb365e7602bf0321eb
11111 <skorodinskaya01@gmail.com>
2021-07-30 00:24:55 +0300

    Lesson 7

---
+++ Lesson7/main.js
@@ -0,0 +1,50 @@
+function wakeUp(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (time > 8) {
+                reject();
+            } else {
+                resolve()
+            }
+        }, 1000)
+    })
+}
+
+function haveBreakfast () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 500)
+    })
+}
+
+wakeUp(7)
+    .then((value) => {
+console.log('Breakfast is ready. Enjoy your meal.')
+    })
+.catch(reason => {
+    console.log('You stay without meal')
+})
+
+function takeAShower() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (wakeUp(reject)) {
+                reject();
+            } else {
+                resolve()
+            }
+        }, 2000)
+    })
+}
+
+takeAShower()
+    .then((value) => {
+        console.log('Take a bath and relax')
+    })
+    .catch(reason => {
+        console.log('You do not have time to take a shower')
+
+    })
+
+
