43ea11b405e9e97ae0c78f97e9a5ee8a6571cb9a
11111 <skorodinskaya01@gmail.com>
2021-08-01 00:14:08 +0300

    Lesson 7 some changes

--- Lesson7/main.js
+++ Lesson7/main.js
@@ -1,66 +1,153 @@
+// Зробити свій розпорядок дня.
+//
+//     У вас має бути більше 10 асинхронних дій з рандомними затримками.
+//     Вам необхідно синхронізувати всі свої дії за допомогою промісів та async await (Код має бути написаний окремо)
+// Затримка має бути НЕ в порядку зростання, а будь яка. При тому ваші дії мають бути синхронізовані.
+//
+//    1. wakeUp - 1000
+//    2. takeAShower - 500
+//    3. gatherOutfit -200
+//    4. breakfast - 1200
+//    5. makeUp - 700
+//    6. doExercises - 2000
+//    7. readABook - 1050
+//    8. studyEnglish - 900
+//    9. doCleaning - 300
+//    10. surfMedias - 450
+
+
 function wakeUp(time) {
     console.log('Morning')
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (time > 8) {
-                reject('I woke up');
+                reject('I overslept');
             } else {
-                resolve('I overslept')
+                resolve('I woke up')
             }
         }, 1000)
     })
 }
 
-wakeUp(9)
-    .then((value) => {
-        console.log('Have breakfast')
+function takeAShower() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('I took a shower')
+
+        }, 500)
+    })
+}
+
+function gatherOutfit() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('I chose classy outfit')
+        }, 200)
+    })
+}
+
+function breakfast() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('I had a tasty breakfast')
+        }, 1200)
+    })
+}
+
+function makeUp(desire) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (desire === 'no') {
+                console.log('I did not have time to do make up');
+                reject('no make up')
+            } else {
+                resolve('I did make up');
+
+            }
+        }, 700)
+    })
+}
+
+function doExercises () {
+ return new Promise((resolve, reject) => {
+     setTimeout(() => {
+         resolve('I worked out')
+     }, 2000)
+ })
+}
+
+function readABook () {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            resolve('I read a book')
+        }, 1050)
+    }))
+}
+function studyEnglish () {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            resolve('I studied English')
+        }, 900)
+    }))
+}
+
+function doCleaning () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve ('I cleaned my room')
+        }, 300)
+    })
+}
+
+function surfMedias () {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            resolve ('I surfed Instagram and TikTok')
+        }, 450)
+    }))
+}
+wakeUp(6)
+    .then(value => {
+        console.log(value);
+        return takeAShower()
+    })
+    .then(value => {
+        console.log(value);
+        return gatherOutfit()
+    })
+    .then(value => {
+        console.log(value);
+        return breakfast()
+    })
+    .then(value => {
+        console.log(value);
+        return makeUp('yes')
+    })
+    .then (value => {
+        console.log(value);
+        return doExercises()
+    })
+    .then (value => {
+        console.log(value);
+        return readABook()
+    })
+    .then (value => {
+        console.log(value);
+        return studyEnglish()
+    })
+
+    .then (value => {
+        console.log(value);
+        return doCleaning()
+    })
+    .then (value =>  {
+        console.log(value);
+        return surfMedias ()
+    })
+    .then(value => {
+        console.log(value);
     })
     .catch(reason => {
-        console.log('You stay without meal')
-    })
-    .then((value) => {
-        function takeAShower() {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (wakeUp.reject) {
-                        reject();
-                    } else {
-                        resolve()
-                    }
-                }, 500)
-            })
-        }
-
-        takeAShower()
-            .then(value => {
-                console.log('Shower')
-            })
-
-            .catch(reason => {
-                console.log('None shower')
-            })
-    })
-    .then((value) => {
-        function gatherOutfit () {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (wakeUp.reject) {
-                        reject();
-                    } else {
-                        resolve () }
-                    }, 1200)
-            })
-        }
-        gatherOutfit ()
-            .then((value) => {
-                console.log('Choose classy outfit')
-            })
-            .catch(reason => {
-                console.log('Put yesterdays clothes')
-            })
-
-            .then ((value) => {
-
-                }
-            )
+        console.warn(reason);
+        console.error('Bad day');
     })
