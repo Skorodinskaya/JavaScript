1e559ebf71992775cf9f64f4e9105e9583a7604a
11111 <skorodinskaya01@gmail.com>
2021-08-03 01:38:13 +0300

    Lesson 7 some changes

---
+++ Project/main1.js
@@ -0,0 +1,37 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(value => {
+        let usersBox = document.getElementsByClassName('users-box') [0]
+        for (const user of value) {
+            let userSelf = document.createElement('p');
+            let userButton = document.createElement('a')
+            userSelf.innerText = `${user.id}. ${user.name}`
+            userButton.href = `user-details.html?userhref=${JSON.stringify(user)}`
+
+
+            userButton.innerText = 'info';
+            usersBox.append(userSelf)
+            userSelf.append(userButton)
+        }
+    })
+// let buttonElement = document.createElement('button');
+// buttonElement.innerText = 'info'
+// buttonElement.href = `user-details.html?user=${JSON.stringify(user)}`
+// buttonElement.onclick = function () {
+//     buttonElement.href = `user-details.html?userLink=${JSON.stringify(user)}`
+// fetch(`https://jsonplaceholder.typicode.com/users`)
+//     .then(value => value.json())
+//     .then(value => {
+//         let infoBox = document.getElementsByClassName('info')[0];
+//         infoBox.innerHTML = '';
+//         for (const informa of value) {
+//             let liInf = document.createElement('li');
+//             liInf.innerText = informa.username
+//             infoBox.append(liInf)
+//         }
+//
+//     })
+// }
+//         userSelf.append(buttonElement)
+//     }
+// })
