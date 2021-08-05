0133372b09e335738800470d9fe0f560dd9467b7
Skorodinskaya <skorodinskaya01@gmail.com>
2021-08-05 21:21:17 +0300

    some changes

--- Project/main1.js
+++ Project/main1.js
@@ -3,14 +3,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => {
         let usersBox = document.getElementsByClassName('users-box') [0]
         for (const user of value) {
+            let total = document.createElement('div');
+            total.className = 'total'
             let userSelf = document.createElement('p');
-            let userLink = document.createElement('a')
+            let userLink = document.createElement('button')
             userSelf.innerText = `${user.id}. ${user.name}`
-            userLink.href = `user-details.html?userhref=${JSON.stringify(user)}`
+            // userLink.href = `user-details.html?userhref=${JSON.stringify(user)}`
+
+            userLink.addEventListener('click', ()=> {
+                location.href = `user-details.html?userhref=${JSON.stringify(user)}`
+            })
 
 
-            userLink.innerText = 'info';
-            usersBox.appendChild(userSelf)
-            userSelf.appendChild(userLink)
+
+                userLink.innerText = 'info';
+            usersBox.appendChild(total)
+            total.append(userSelf,userLink)
         }
     })
